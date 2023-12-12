73915b7cbf0d27a77df6bbf3101c2d8f04e212f9
ssSally111 <2392341633@qq.com>
2023-12-12 22:53:07 +0800

    update:Support for api KillProcess in driver DriverTest

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <windows.h>
 #include <iostream>
+#include <winioctl.h>
 
 #define DRIVER_NAME L"\\\\.\\DriverControlsTest"
+#define IOCTL_TEST CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_BUFFERED, FILE_ANY_ACCESS)
+
+void KernelKillProcess(HANDLE hDevice, ULONG pid)
+{
+	DWORD dwOpt = 0;
+	UCHAR optBuff[8];
+	if (DeviceIoControl(hDevice, IOCTL_TEST, &pid, sizeof(ULONG), optBuff, 8, &dwOpt, NULL))
+	{
+		std::cout << "IOCTL_TEST result:";
+		for (int i = 0; i < (int)dwOpt; i++)
+		{
+			std::cout << std::hex << (int)optBuff[i];
+		}
+	}
+
+	std::cout << std::endl << "KernelKillProcess PID:" << pid << std::endl;
+}
 
 int main(int argc, char* argv[])
 {
@@ -19,16 +37,21 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	UCHAR buff[10];
-	ULONG ulRead;
-	if (ReadFile(hDevice, buff, 10, &ulRead, NULL))
-	{
-		std::cout << "read length:" << ulRead << std::endl;
-		for (int i = 0; i < (int)ulRead; i++)
-		{
-			std::cout << std::hex << (int)buff[i] << " ";
-		}
-	}
+	//UCHAR optBuff[8];
+	//ULONG ulRead;
+	//if (ReadFile(hDevice, buff, 10, &ulRead, NULL))
+	//{
+	//	std::cout << "read length:" << ulRead << std::endl;
+	//	for (int i = 0; i < (int)ulRead; i++)
+	//	{
+	//		std::cout << std::hex << (int)buff[i] << " ";
+	//	}
+	//}
+
+	ULONG pid;
+	std::cout << "please enter pid..." << std::endl;
+	scanf_s("%d", &pid);
+	KernelKillProcess(hDevice, pid);
 
 	CloseHandle(hDevice);
 	return 0;
